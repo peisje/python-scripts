2a8974bedb7a22ad1875fac91bed0e64320b22ee
peisje <darjakovalenko5@gmail.com>
2025-05-07 11:15:21 +0300

    Add files via upload

---
+++ proccessmenu.py
@@ -0,0 +1,21 @@
+import subprocess
+
+def menu():
+    while True:
+        print("\nMENU")
+        print("1 -  (Ping)")
+        print("2 -  (Tasklist)")
+        print("3 - (Exit)")
+        choice = input("Vali tegevus  ")
+
+        if choice == '1':
+            subprocess.run(["python", "importostime.py"])
+        elif choice == '2':
+            subprocess.run(["python", "tasklist.py"])
+        elif choice == '3':
+            print("Väljun programmist...")
+            break
+        else:
+            print("Vigane valik. Proovi uuesti.")
+
+menu()
